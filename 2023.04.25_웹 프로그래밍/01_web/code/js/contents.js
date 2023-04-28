4e5982926ed87ed66746f751f7bf69aa94f6cac7
Jihwan98 <76936390+Jihwan98@users.noreply.github.com>
2023-04-28 16:50:49 +0900

    Add files via upload

--- 2023.04.25_웹 프로그래밍/01_web/code/js/contents.js
+++ 2023.04.25_웹 프로그래밍/01_web/code/js/contents.js
@@ -1,33 +1,31 @@
-(function () {
-    var msgs = {
-        login: 'Login Your Account.',
-        noAccount: 'Tdere is no Account.',
-    };
+(function(){
     var Event = {
-        getContents: function () {
-            $('.search').on('click', function () {
+        getContents: function(){
+            $('button.search').on('click', function(){
                 var addr = $('.input-address').val();
-                var url = 'https://apis.zigbang.com/v2/search?leaseYn=N&q=' + addr + '&serviceType=원룸'
-                $.getJSON(url, function (json) {
-                    // console.log(json.items);
+                var url = 'https://apis.zigbang.com/v2/search?leaseYn=N&q=';
+                    url = url + addr + '&serviceType=원룸';
+                console.log(url);
+                $.getJSON(url, function(response){
+                    console.log(response.items);
                     var tag = '';
-                    for(var i = 0; i < json.items.length; i++){
-                        // console.log(json.items[i])
+                    for(var i = 0; i < response.items.length; i++){
+                        console.log(response.items[i])
+                        var item = response.items[i];
                         tag += '<tr>';
                         tag += '    <td>' + (i + 1) + '</td>';
-                        tag += '    <td>' + json.items[i].id + '</td>';
-                        tag += '    <td>' + json.items[i].name + '</td>';
-                        tag += '    <td>' + Math.round(Number(json.items[i].lat) * 100) / 100 + '</td>';
-                        tag += '    <td>' + Math.round(Number(json.items[i].lng) * 100) / 100 + '</td>';
-                        tag += '    <td>' + json.items[i].description + '</td>';
+                        tag += '    <td>' + item.id + '</td>';
+                        tag += '    <td>' + item.name + '</td>';
+                        tag += '    <td>' + (Math.round(Number(item.lat) * 100) / 100) + '</td>';
+                        tag += '    <td>' + (Math.round(Number(item.lng) * 100) / 100) + '</td>';
+                        tag += '    <td>' + item.description + '</td>';
                         tag += '</tr>';
-                    }
-                    $('.table-wrap > table > tbody').html(tag);
-                    $('.table-wrap').show();
+                    };
+                    $('.table-wrap>table>tbody').html(tag);
+                    $('.table-wrap').show(200);
                 });
             });
         }
     };
     Event.getContents();
-    // $('.search').click();
 })();
--- 2023.04.25_웹 프로그래밍/01_web/code/js/join.js
+++ 2023.04.25_웹 프로그래밍/01_web/code/js/join.js
@@ -1,29 +1,30 @@
-(function() {
+(function(){
     var msgs = {
-        checkPw: 'Check Your Password.',
-        joinAccount: 'Joined Your Account.',
-    };
+        checkPW: 'Check Your Password!',
+        joinAccount: 'Joined Your Account!',
+    }
     var Event = {
-        join: function() {
-            $('#join-btn').on('click', function() {
-                var params = {
+        join: function(){
+            console.log('Event Join');
+            $('#join-btn').on('click', function(){
+                var params ={
                     id: $('#email').val(),
                     pw: $('#pw').val(),
                     ckpw: $('#ckpw').val(),
                     addr: $('input[name="addr"]:checked').val(),
                     birth: $('#birth').val(),
                     subscription: $('#subscription').val(),
-                };
+                }
                 console.log(params);
-                if(params.pw != params.ckpw){
+                if(params.pw !== params.ckpw){
                     console.log('check password!');
-                    $('.join-wrap>.msg').text(msgs.checkPw).show();
-                } else {
-                    console.log(params);
-                    $('.join-wrap>.msg').text(msgs.joinAccount).show();
-                };
+                    $('.join-wrap > .msg').text(msgs.checkPW).show();
+                }else{
+                    console.log('joined!');
+                    $('.join-wrap > .msg').text(msgs.joinAccount).show();
+                }
             });
         }
-    };
+    }
     Event.join();
 })();
